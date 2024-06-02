34a6eb12bb5897fedd81d10d524955f56b6febef
Davi C. Pes. <davicalo2d@gmail.com>
2024-06-02 13:02:24 -0300

    send_message_ok

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-for (let i = 0; i < 60; i++) {
+const chat = document.getElementById('messanges');
+const form = document.getElementById('form-message');
+const input = document.getElementById('messange-input');
 
-    document.getElementById("cu").textContent = i;
+
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const element = createMessage();
+
+  chat.appendChild(element);
+  input.value = '';
+
+  chat.scrollTop = chat.scrollHeight;
+})
+
+
+const createMessage = () => {
+    const Message = document.createElement('div');
+    const Name = document.createElement('span');
+    const Content = document.createElement('span');
+
+    Name.textContent = "Davi Calô";
+    Content.textContent = input.value;
+    
+    Message.appendChild(Name);
+    Message.appendChild(Content);
+    console.log(Message);
+
+    return Message;
 }
-
-
